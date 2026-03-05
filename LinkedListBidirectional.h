f10063c0f92c41108a1142405bc686555810da83
Ervin Senkevich <146520901+Ervin518@users.noreply.github.com>
2026-03-05 08:34:26 +0100

    Update LinkedListBidirectional.h

--- LinkedListBidirectional.h
+++ LinkedListBidirectional.h
@@ -1,24 +1,23 @@
 #ifndef LINKEDLISTBIDIRECTIONAL_H
 #define LINKEDLISTBIDIRECTIONAL_H
+#include "Node.h"
 
-#include "Wezel.h"
-
-typedef struct {
+struct LinkedList {
     struct Node* head;
-} LinkedList;
+};
 
-LinkedList* create_list();
-void free_list(LinkedList* list);
+struct LinkedList* create_list();
+void free_list(struct LinkedList* list);
 
-void print(LinkedList* list);
-void save(LinkedList* list, const char* filename);
-void load_list(LinkedList* list, const char* filename);
+void print(struct LinkedList* list);
+void save(struct LinkedList* list, const char* filename);
+void load_list(struct LinkedList* list, const char* filename);
 void print_actions();
 
-struct Node* push(LinkedList* list, struct Material* mat);
-struct Node* pop(LinkedList* list);
-struct Node* pop_at(LinkedList* list, int index);
-struct Node* push_at(LinkedList* list, struct Material* mat, int index);
-struct Node* get_element(LinkedList* list, int index);
+struct Node* push(struct LinkedList* list, struct Material* mat);
+struct Node* pop(struct LinkedList* list);
+struct Node* push_at(struct LinkedList* list, struct Material* mat, int index);
+struct Node* pop_at(struct LinkedList* list, int index);
+struct Node* get_element(struct LinkedList* list, int index);
 
 #endif
